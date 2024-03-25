03602d73ea18a0616748e28757721bb8c7253a6a
Abhay Singh Gill <abhaygill017@gmail.com>
2024-03-25 19:05:07 +0530

    veux: Add support of OnePlus Dolby
    - Add OnePlus dolby fron neoteric
    - Move media_codecs_blair in device tree
    - Sync dolby effect
    - Track audio_effects from device tree and drop audio effect types
    - Enable HW_ACCELERATED_EFFECTS & dolby.ds2 props
    - Add dolby repo in vendorsetup.sh
    
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,8 +3,10 @@ rm -rf kernel/xiaomi
 rm -rf device/xiaomi/veux-kernel
 rm -rf vendor/xiaomi/miuicamera-veux
 rm -rf vendor/xiaomi/miuicamera
+rm -rf hardware/dolby
 git clone --depth=1 https://gitea.com/ThePrateekBhatia/vendor_xiaomi_veux.git vendor/xiaomi/veux
 git clone --depth=1 https://github.com/crdroidandroid/android_kernel_oneplus_sm8350.git kernel/xiaomi/sm6375
 git clone --depth=1 https://github.com/ThePrateekBhatia/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
 git clone --depth=1 https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux.git -b main --depth=1 vendor/xiaomi/miuicamera-veux
 git clone --depth=1 https://github.com/Positron-B/vendor_xiaomi_miuicamera.git -b main --depth=1 vendor/xiaomi/miuicamera
+git clone --depth=1 https://github.com/FlamingoOS-Devices/hardware_dolby.git hardware/dolby
