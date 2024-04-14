098925be7bc6e5a6836bf14a24cb863d85be6983
Prateek Bhatia <bhatiaprateek111@gmail.com>
2024-04-14 22:48:42 +0530

    veux: cleanup and reorder files, switched to perf vendor and MiUi cam repo
    Signed-off-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,10 @@
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf device/xiaomi/veux-kernel
-git clone --depth=1 https://gitea.com/Karan-Frost/vendor_xiaomi_veux.git vendor/xiaomi/veux
-git clone --depth=1 https://github.com/dereference23/kernel_xiaomi_sm6375.git kernel/xiaomi/sm6375
+rm -rf vendor/xiaomi/miuicamera-veux
+rm -rf vendor/xiaomi/miuicamera
+git clone --depth=1 https://gitea.com/ThePrateekBhatia/vendor_xiaomi_veux.git vendor/xiaomi/veux
+git clone --depth=1 https://github.com/crdroidandroid/android_kernel_oneplus_sm8350.git kernel/xiaomi/sm6375
 git clone --depth=1 https://github.com/ThePrateekBhatia/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
-git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
+git clone --depth=1 https://github.com/Positron-B/vendor_xiaomi_miuicamera-veux.git -b main --depth=1 vendor/xiaomi/miuicamera-veux
+git clone --depth=1 https://github.com/Positron-B/vendor_xiaomi_miuicamera.git -b main --depth=1 vendor/xiaomi/miuicamera
